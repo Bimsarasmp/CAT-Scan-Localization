c1dce775f11cdc323e48a6acf3862ace4ec999a2
edojatheophilus <139920113+edojatheophilus@users.noreply.github.com>
2023-08-16 16:27:16 -0400

    Update UserInterface.py

--- API/UserInterface.py
+++ API/UserInterface.py
@@ -3,11 +3,15 @@ from PIL import Image
 import numpy as np
 import requests
 
+# Function to send features to an API for localization prediction
 def decode(features):
+    # Create the request body
     body = {"features":features}
     try:
+        # Send a POST request to the localization prediction API
         response = requests.post(url="http://localhost:105/sliceLocalizationPrediction", json=body)
     except requests.exceptions.ConnectionError as e:
+         # Handle connection error to the API
         st.text("API Connection Failed")
         return []
 
@@ -15,16 +19,22 @@ def decode(features):
         imageResponse = np.array(response.json()['image'])
         return imageResponse
     else:
+        # Handle API call failure
         st.text("API Call Failed")
 
 def main():
+     # Streamlit app title and user input section
     st.title("CATSCAN Localization")
     featuresInput = st.text_input("Enter comma separted value")
+    
+    # Button to trigger localization prediction
     if st.button("Localize"):
         features = list(map(float,featuresInput.title().split(",")))
-        img = decode(features)
+        img = decode(features) # Call the localization prediction function
+
         if len(img)>0:
+            # Display the predicted image
             st.image(img, width=391)
 
 if __name__ == "__main__":
-    main()
+    main() # Run the Streamlit app
